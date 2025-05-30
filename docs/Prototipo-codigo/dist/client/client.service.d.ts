f8d43d320e05ecf6c8e3ac75ada004d6fa9de7a9
mateuscavati <mateusmcs.profissional@outlook.com>
2025-05-30 07:02:24 -0300

    projeto

--- docs/Prototipo-codigo/dist/client/client.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { ArtService } from '../art/art.service';
-export declare class ClientService implements OnModuleInit {
-    private readonly artService;
-    constructor(artService: ArtService);
-    onModuleInit(): void;
-}
