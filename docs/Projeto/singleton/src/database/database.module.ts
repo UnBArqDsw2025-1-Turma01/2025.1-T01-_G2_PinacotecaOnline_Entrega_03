1878e0a2a39ef2e1c193d7db2ececbb8b0f61a9b
Felipe de Sousa <felipescoelho517@gmail.com>
2025-05-31 17:17:52 +0000

    removendo projeto da branch

--- docs/Projeto/singleton/src/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { DatabaseConnectionPoolService } from './database-connection-pool.service';
-
-@Global() // Torna os provedores deste módulo disponíveis globalmente
-@Module({
-  providers: [DatabaseConnectionPoolService],
-  exports: [DatabaseConnectionPoolService], // Exporta para que outros módulos possam injetá-lo
-})
-export class DatabaseModule {}
