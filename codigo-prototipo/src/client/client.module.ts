e9a82bb9377879bc89fa4ef30a37b3ab3c444adf
mateuscavati <mateusmcs.profissional@outlook.com>
2025-06-02 05:01:30 -0300

    .

--- codigo-prototipo/src/client/client.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClientService } from './client.service';
-import { ArtModule } from '../art/art.module';
-
-@Module({
-  imports: [ArtModule],
-  providers: [ClientService],
-})
-export class ClientModule {}
--- codigo-prototipo/src/main.ts
+++
@@ -1,9 +0,0 @@
-// src/main.ts
-
-import { NestFactory } from '@nestjs/core';
-import { ClientModule } from './client/client.module';
-
-async function bootstrap() {
-  await NestFactory.createApplicationContext(ClientModule);
-}
-bootstrap();
